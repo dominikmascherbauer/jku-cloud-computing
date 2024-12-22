f1944ee7e4974139b6c305862d7124a9fa042ffc
MichaelHaas99 <michael.haas0612@gmail.com>
2024-12-22 11:20:23 +0100

    Finished database service

--- microservices/database/routes/api.js
+++ microservices/database/routes/api.js
@@ -1,59 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../database');
-const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3001 });
-
-function broadcast(info){
-  for(broadcastTarget of wss.clients){
-    if(broadcastTarget.readyState === WebSocket.OPEN){
-      broadcastTarget.send(JSON.stringify(info))
-    }
-  }
-}
-
-wss.on('connection', (ws, req) => {
-  console.log(`WS Client connected, we now have ${wss.clients.size} connected clients`)
-  ws.on('close', () => {
-    console.log(`Client disconnected, we now have ${wss.clients.size} connected clients`)
-  });
-});
-
-// Middleware to check if user is logged in
-function isLoggedIn(req, res, next) {
-  if (!req.jwtProvided) {
-    console.log("Denied: Authentication required");
-    return res.status(401).send('Authentication required');
-  } else if (req.jwtVerifyError || req.jwtExpired) {
-    console.log("Denied: Invalid authentication token");
-    return res.status(401).send('Invalid authentication token');
-  }
-  next();
-}
-
-function isAdmin(req, res, next) {
-  if (req.jwtPayload && req.jwtPayload.userIsAdmin) {
-    next();
-  } else {
-    console.log("Denied: Admin privileges required");
-    res.status(403).send('Admin privileges required');
-  }
-}
-
-// Get logged in user information
-router.get('/user/information', async (req, res) => {
-  try {
-    if(!req.jwtProvided ||req.jwtVerifyError || req.jwtExpired) res.send()
-    else res.send({name: req.jwtPayload.name, isAdmin: req.jwtPayload.userIsAdmin})
-  } catch (error) {
-    res.status(500).send('Error fetching users information');
-    console.error(error)
-  }
-});
 
 // Get all users endpoint
-router.get('/user/all', isLoggedIn, isAdmin, async (req, res) => {
+router.get('/user/all',  async (req, res) => {
   try {
     const users = await db.user.findAllUsers()
     res.json(users);
@@ -64,7 +15,7 @@ router.get('/user/all', isLoggedIn, isAdmin, async (req, res) => {
 });
 
 // Delete a user
-router.delete('/user/delete/:mail', isLoggedIn, isAdmin, async (req, res) => {
+router.delete('/user/delete/:mail',  async (req, res) => {
   try {
     const ack = await db.user.deleteUserByMail(req.params.mail)
     res.send()
@@ -75,7 +26,7 @@ router.delete('/user/delete/:mail', isLoggedIn, isAdmin, async (req, res) => {
 });
 
 // Update a users role
-router.post('/user/:mail/:newRole', isLoggedIn, isAdmin, async (req, res) => {
+router.post('/user/:mail/:newRole', async (req, res) => {
   try {
     const ack = await db.user.updateUserRoleByMail(req.params.mail, req.params.newRole)
     res.send()
@@ -97,7 +48,7 @@ router.get('/article/all', async (req, res) => {
 });
 
 // Get all courts including reservations and unavailabilites on a certain date
-router.get('/court/all/:date', isLoggedIn, async (req, res) => {
+router.get('/court/all/:date', async (req, res) => {
   try {
     const courts = await db.court.findAllCourtsWithReservationsUnavailabilites(req.params.date);
     res.json(courts)
@@ -109,7 +60,7 @@ router.get('/court/all/:date', isLoggedIn, async (req, res) => {
 
 
 // Get all courts
-router.get('/court/all', isLoggedIn, async (req, res) => {
+router.get('/court/all',  async (req, res) => {
   try {
     const courts = await db.court.findAllCourts()
     res.json(courts);
@@ -120,7 +71,7 @@ router.get('/court/all', isLoggedIn, async (req, res) => {
 });
 
 // Start watering on a court
-router.post('/court/startWatering/:courtId', isLoggedIn, async (req, res) => {
+router.post('/court/startWatering/:courtId',  async (req, res) => {
   try {
     const ack = await db.court.startWatering(req.params.courtId)
     broadcast({entity: 'watering', op: 'start', data: {courtId: req.params.courtId, lastWatering: ack}})
@@ -132,7 +83,7 @@ router.post('/court/startWatering/:courtId', isLoggedIn, async (req, res) => {
 });
 
 // Stop watering on a court
-router.post('/court/stopWatering/:courtId', isLoggedIn, async (req, res) => {
+router.post('/court/stopWatering/:courtId',  async (req, res) => {
   try {
     const ack = await db.court.stopWatering(req.params.courtId)
     broadcast({entity: 'watering', op: 'stop', data: {courtId: req.params.courtId}})
@@ -144,7 +95,7 @@ router.post('/court/stopWatering/:courtId', isLoggedIn, async (req, res) => {
 });
 
 // Get personal reservations on a court on a date
-router.get('/court/reservation/:date', isLoggedIn,  async (req, res) => {
+router.get('/court/reservation/:date',  async (req, res) => {
   try {
     const reservations = await db.court.findPersonalReservations(req.jwtPayload.id, req.params.date)
     res.json(reservations)
@@ -155,7 +106,7 @@ router.get('/court/reservation/:date', isLoggedIn,  async (req, res) => {
 });
 
 // Add a reservation on a court
-router.post('/court/reservation/add', isLoggedIn,  async (req, res) => {
+router.post('/court/reservation/add',  async (req, res) => {
   try {
     req.body.userId = req.jwtPayload.id;
     const ack = await db.court.addReservation(req.body)
@@ -168,7 +119,7 @@ router.post('/court/reservation/add', isLoggedIn,  async (req, res) => {
 });
 
 // Delete a reservation on a court
-router.delete('/court/reservation/delete/:reservationId', isLoggedIn, async (req, res) => {
+router.delete('/court/reservation/delete/:reservationId', async (req, res) => {
   try {
     const reservation = await db.court.deleteReservation(req.params.reservationId)
     broadcast({entity: 'reservation', op: 'delete', data: {courtId: reservation.courtId, startHour: reservation.startHour}})
