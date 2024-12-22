a0b0e466488f97cc56d64a7783449482f9adbda6
MichaelHaas99 <michael.haas0612@gmail.com>
2024-12-22 15:39:04 +0100

    JWT _Secret not necessary anymore in data service

--- microservices/data/app.js
+++ microservices/data/app.js
@@ -3,12 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var jwt = require('jsonwebtoken');
-const jwtSecret = process.env.JWT_SECRET;
-if (!jwtSecret) {
-    console.error('FATAL ERROR: JWT_SECRET env var is not defined.');
-    process.exit(1); // Exit the process with an error code
-}
 
 var apiRouter = require('./routes/api');
 
